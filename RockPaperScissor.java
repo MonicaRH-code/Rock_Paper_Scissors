5d5bb193200797aabb8ea34be13774687cbc5890
MonicaRH-code <76511932+MonicaRH-code@users.noreply.github.com>
2023-11-25 20:48:13 -0500

    Update RockPaperScissor.java

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -1,50 +1,50 @@
 import java.util.Scanner;
 import java.util.Random;
 public class RockPaperScissor {
-
     public static void main(String args[])
     {
         Scanner scan=new Scanner(System.in);
         Random rand = new Random();
-        System.out.println("Wecome to the amazing game of Rock-Paper-Scissor");
+        System.out.println("Wecome to the amazing game of ROCK-PAPER-SCISSOR");
         System.out.println("Enter user choice");
-        String u=scan.nextLine();
+        String l=scan.nextLine();
+        String u=l.toUpperCase(); 
         String c;
         int n=rand.nextInt(3) ;
         if(n==0)
         {
-        c= "Rock";
+        c= "ROCK";
         }
         else if(n==1)
         {
-            c="Paper";
+            c="PAPER";
         }else
         {
-            c="Scissor";
+            c="SCISSOR";
         }
         System.out.println("Computer's choice ->" + c);
 
-        if(u.equals("Rock")  && c.equals("Paper" ))
+        if(u.equals("ROCK")  && c.equals("PAPER" ))
         {
             System.out.println("computer wins the game");
         }
-        else if(u.equals("Rock")&& c.equals("Scissor"))
+        else if(u.equals("ROCK")&& c.equals("SCISSOR"))
         {
           System.out.println("User wins the game");
         }
-        else if(u.equals("Paper") && c.equals("Scissor"))
+        else if(u.equals("PAPER") && c.equals("SCISSOR"))
         {
             System.out.println("Computer wins the game");
         }
-        else if(u.equals("Paper") && c.equals("Rock"))
+        else if(u.equals("PAPER") && c.equals("ROCK"))
         {
             System.out.println("User wins the game");
         }
-        else if(u.equals("Scissor") && c.equals("Rock"))
+        else if(u.equals("SCISSOR") && c.equals("ROCK"))
         {
             System.out.println("Computer wins the game");
         }
-        else if(u.equals("Scissor") && c.equals("Paper"))
+        else if(u.equals("SCISSOR") && c.equals("PAPER"))
         {
             System.out.println("User wins the game");
         }
